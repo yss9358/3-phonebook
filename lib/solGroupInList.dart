159a82138448223ffc9f30d630b5309aeb5b7f24
ImSol-k <tiq0015@naver.com>
2024-04-30 15:46:51 +0900

    소리 폴더추가

---
+++ lib/solGroupInList.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GroupInList extends StatelessWidget {
+  const GroupInList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("00그룹")),
+    );
+  }
+}
